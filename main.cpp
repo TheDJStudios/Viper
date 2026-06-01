3522474f34a9403366ae4005606d89452ca495e9
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2026-06-01 17:21:05 -0500

    numbers :D

--- main.cpp
+++ main.cpp
@@ -4,11 +4,23 @@
 
 int main() {
     std::cout << "Welcome to Viper executable. select a function to get started:\n";
-    std::string command;
+    int command;
     std::cout << "[1] Compile\n";
     std::cout << "[2] Interpret\n";
     std::cout << "\n";
     std::cout << "Viper> ";
-    std::getline(std::cin, command);
-    std::cout << command << " Either doesnt exist or is a stub" << std::endl;
+    std::cin >> command;
+    if (command == 1) {
+        std::cout << "\n" << "Viper compiler" << "\n";
+        std::cout << "Stub";
+        return 1;
+    } else if (command == 2) {
+        std::cout << "\n" << "Interpreter" << "\n";
+        std::cout << "Stub";
+        return 1;
+    } else {
+        std::cout << "\n";
+        std::cout << "Not a function";
+        return 2;
+    }
 }
