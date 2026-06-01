df46c9b097e605646a2c020e7b7d48eb3715ecf8
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2026-06-01 01:12:00 -0500

    efficency

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+    std::string mode = argv[1];
     if (argc < 2) {
-        std::cout << "Usage: 'viper compile/interp'";
+        std::cout << "Usage: 'viper compile/interp'\n";
         return 1;
     }
-    if (std::string(argv[1]) == "compile") {
-        std::cout << "Compile is currently a stub";
-    } else if (std::string(argv[1]) == "interp") {
-        std::cout << "Interp is currently a stub";
+    if (mode == "compile") {
+        std::cout << "Compile is currently a stub\n";
+    } else if (mode == "interp") {
+        std::cout << "Interp is currently a stub\n";
     }
 }
